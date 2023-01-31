8e371a96d098c016c4e7e2d6902432964d49fe0a
LukaVieira1 <lukavieira14@gmail.com>
2023-01-31 19:27:44 -0300

    image resize and validation with no image

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -15,12 +15,20 @@ const MovieCard = (props) => {
         <Card variant="filled" borderColor="blue" maxW="sm">
           <CardBody>
             <Image
-              src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${image}`}
+              src={
+                image !== null
+                  ? `https://image.tmdb.org/t/p/w185_and_h278_bestv2${image}`
+                  : "https://picsum.photos/200"
+              }
+              width="278px"
+              height="278px"
               alt={title}
               borderRadius="lg"
             />
             <Stack mt="6" spacing="3">
-              <Heading size="md">{title}</Heading>
+              <Heading maxW="278px" size="md">
+                {title}
+              </Heading>
               <Text>Data de lançamento: {formatLongDate(releaseDate)}</Text>
             </Stack>
           </CardBody>
