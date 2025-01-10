018b798670850085df0d54b22fc002b7943fe4c4
LukaVieira1 <lukavieira14@gmail.com>
2025-01-10 07:19:15 -0300

    add: new bg

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,10 @@ import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Component {...pageProps} />
+    <div className="min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 overflow-x-hidden">
+      <div className="max-w-[1920px] mx-auto">
+        <Component {...pageProps} />
+      </div>
     </div>
   );
 }
