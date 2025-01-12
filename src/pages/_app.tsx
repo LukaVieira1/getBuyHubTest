721b53eda603cd60895efdb6843004c6f2df4daa
LukaVieira1 <lukavieira14@gmail.com>
2025-01-12 10:53:19 -0300

    add: UI Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { UIProvider } from "@/contexts/UIContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 overflow-x-hidden">
-      <div className="max-w-[1920px] mx-auto">
-        <Component {...pageProps} />
+    <UIProvider>
+      <div className="min-h-screen bg-gradient-to-b from-gray-900 via-gray-800 to-gray-900 overflow-x-hidden">
+        <div className="max-w-[1920px] mx-auto">
+          <Component {...pageProps} />
+        </div>
       </div>
-    </div>
+    </UIProvider>
   );
 }
