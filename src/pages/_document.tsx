f07262b070bd1bd094e834714d905be3990e43eb
LukaVieira1 <lukavieira14@gmail.com>
2025-01-14 08:33:31 -0300

    add: new name and fav icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <title>WikiMovies</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
